6f04fdd89136f7e49eccf2e7d29bc561f7bf5843
Abhishekmar <160823251+Abhishekmar@users.noreply.github.com>
2024-02-28 10:24:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = os.environ.get("API_ID", "21414727")
 
 API_HASH = os.environ.get("API_HASH", "b4135f6b8cd476d931e78787a0cb77c1")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6511248244:AAFrpZ6J_ZwEzEVbs9jwM7S1_9jb-O5_oWI") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7093405647:AAEI4naYKiJYjX2smeVhMFHbz_TJ70CfpJQ") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "Movilious") 
 
@@ -19,13 +19,13 @@ FORCE_SUB = os.environ.get("FORCE_SUB", "Movilious")
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
              # Ask Doubt on telegram @KingVJ01
 
-DB_NAME = os.environ.get("DB_NAME", "renameabhibot")     
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")     
 
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://Abhishekq76:84078042@cluster0.s3319zt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://abhishekkum:PassMongo@cluster0.cfviomt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/b13f89e74f5d221a94536.jpg")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6796937630').split()]
 
